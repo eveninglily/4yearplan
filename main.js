c2db770ac0eaeb24eb0d30dfb8bd15d61b8e43b1
Jacob Cohen <jacobcohen@Jacobs-MacBook-Pro.local>
2017-04-08 08:19:15 -0400

    Autofill w/ broken textbox

--- main.js
+++ main.js
@@ -1,3 +1,28 @@
 jQuery(".btn.btn-lg.btn-default").click(function() {
 	console.log("Future AJAX event will be here!");
 	});
+	
+/*jQuery( function() {
+	jQuery( "#speed" ).selectmenu();
+	  } );
+  */
+    jQuery( function() {
+    var availableMajors = [
+      "Computer Science (CMSC)",
+      "Mathematics (MATH)",
+      "Internet Studies (MEME)",
+    ];
+    jQuery( "#tags" ).autocomplete({
+      source: availableMajors
+    });
+  } );
+
+/*    function prepareEventHandlers() {
+  	jQuery( "#speed" ).hide();
+  };
+  
+  window.onload = function() {
+  	console.log("Huh?");
+  	prepareEventHandlers;
+  };
+  */

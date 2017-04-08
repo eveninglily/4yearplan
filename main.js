15aab98794cedc7689164ad00690e3140ec0ce8d
Jacob Cohen <jacobcohen@Jacobs-MacBook-Pro.local>
2017-04-08 14:41:18 -0400

    Tabs update

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-jQuery(".btn.btn-lg.btn-default").click(function() {
+/*jQuery(".btn.btn-lg.btn-default").click(function() {
 	console.log("Future AJAX event will be here!");
-	});
+	});*/
 	
 /*jQuery( function() {
 	jQuery( "#speed" ).selectmenu();
@@ -26,3 +26,24 @@ jQuery(".btn.btn-lg.btn-default").click(function() {
   	prepareEventHandlers;
   };
   */
+
+//Year seperator tabs
+  $( function() {
+    $( ".col-md-8.col" ).tabs();
+  });
+
+//Autocomplete majors
+  $( function() {
+    function log( message ) {
+      $( "<div>" ).text( message ).prependTo( "#log" );
+      $( "#log" ).scrollTop( 0 );
+    }
+ 
+    $( "#majors" ).autocomplete({
+      source: "search.php",
+      minLength: 2,
+      select: function( event, ui ) {
+        log( "Selected: " + ui.item.value + " aka " + ui.item.id );
+      }
+    });
+  } );

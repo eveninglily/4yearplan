cc120e0e1e91a0f3ae4162e8ef5cabcea6ad4516
Evan McIntire <mcintire.evan@gmail.com>
2017-04-08 00:49:02 -0400

    Begin adding generation code
    * Function to get list of all prereqs of a class (going all the way up)
    * Function to check if a classes prereqs have been satisfied before a semester
    * Function to attempt to add a class as early as possible in the schedule

---
+++ index.js
@@ -0,0 +1,75 @@
+var fs = require('fs');
+
+var major = JSON.parse(fs.readFileSync('majors/test.json', 'utf8'));
+var semesters = [
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    []
+];
+
+var requirements = major.requirements;
+
+for(var key in requirements) {
+    if(requirements.hasOwnProperty(key)) {
+        //console.log(requirements[key]);
+    }
+}
+
+function getFullPrereqs(name) {
+    var full = [];
+    var cl;
+    for(var key in requirements) {
+        if(requirements.hasOwnProperty(key)) {
+            if(requirements[key].name == name) {
+                cl = requirements[key];
+                full = requirements[key].prereqs;
+                for(var req in requirements[key].prereqs) {
+                    if(requirements[key].prereqs.hasOwnProperty(req)) {
+                        full = full.concat(getFullPrereqs(requirements[key].prereqs[req]));
+                    }
+                }
+            }
+        }
+    }
+    return full.filter(function(item, pos) {
+        return full.indexOf(item) == pos;
+    });
+}
+
+function canTakeInSemester(name, semester) {
+    var classes = [];
+    for(var i = 0; i < semester; i++) {
+        classes = classes.concat(semesters[i]);
+    }
+
+    var reqs = getFullPrereqs(name);
+
+    reqs = reqs.filter(function(item) {
+        return classes.indexOf(item) == -1;
+    });
+    return reqs.length == 0;
+}
+
+//returns false if it couldnt
+function addEarliest(name) {
+    for(var i = 0; i < 8; i++) {
+        if(canTakeInSemester(name, i)) {
+            semesters[i].push(name);
+            return true;
+        }
+    }
+    return false;
+}
+
+for(var key in requirements) {
+    if(requirements.hasOwnProperty(key)) {
+        //console.log(getFullPrereqs(requirements[key].name));
+        console.log(addEarliest(requirements[key].name));
+    }
+}
+console.log(semesters);
